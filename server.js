ab66b8d6091dac6e821ef31b70841b6fa6354121
Morfeus <kuthykymaell@gmail.com>
2026-04-17 18:05:36 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 require("dotenv").config();
 
+const express = require("express");
+const app = express();
+
 const user = process.env.MONGO_USER;
 const password = process.env.MONGO_PASSWORD;
 
-console.log(user);
-console.log(password);
+console.log("USER:", user);
+console.log("PASS:", password);
+
+// rota básica
+app.get("/", (req, res) => {
+    res.send("Servidor online 🚀");
+});
+
+// importante: usar porta do Render
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
