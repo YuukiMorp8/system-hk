31a94f84abb677b6428acd11e54638f00ee147c3
Morfeus <kuthykymaell@gmail.com>
2026-04-17 21:59:08 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,41 @@
-require("dotenv").config();
+const dotenv = require("dotenv");
+dotenv.config();
 
 const express = require("express");
 const app = express();
 
-const user = process.env.MONGO_USER;
-const password = process.env.MONGO_PASSWORD;
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-console.log("USER:", user);
-console.log("PASS:", password);
+async function connectToDatabase() {
+    const client = new MongoClient(process.env.DB_URL);
+    await client.connect();
+    return client;
+}
+connectToDatabase().then(client => {
+    console.log("Conectado ao MongoDB");
+    const db = client.db("BancOAfW");
+    const usersCollection = db.collection("users");
+    app.post("/login", express.json(), async (req, res) => {
+        const { username, password } = req.body;
+        const user = await usersCollection.findOne({ username,
+            password });
+        if (user) {
+            res.json({ success: true, message: "Login bem-sucedido" });
+        }
+        else {
+            res.json({ success: false, message: "Credenciais inválidas" });
+        }
+    });
+}).catch(err => {
+    console.error("Erro ao conectar ao MongoDB:", err);
+});
 
-// rota básica
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/Servidor/Login.html");
+ res.sendFile(__dirname + "/Sistema/Login.html");
 });
 
-// importante: usar porta do Render
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+app.listen(3000, () => {
+ console.log("Servidor rodando");
 });
+
