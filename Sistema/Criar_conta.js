6ba6c00e43a08c74c5f8d18ea094bb93ea0c5cfe
Morfeus <kuthykymaell@gmail.com>
2026-04-18 01:41:47 -0300

    Update Criar_conta.js

--- Sistema/Criar_conta.js
+++ Sistema/Criar_conta.js
@@ -9,7 +9,18 @@ criarconta.addEventListener("click", function() {
     let nome = nomeinput.value;
     let email = emailinput.value; 
     let senha = senhainput.value; 
-    console.log("Nome:", nome);
-    console.log("Email:", email);
-    console.log("Senha:", senha);
+fetch("/criar-conta", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ nome: nome, email: email, senha: senha })
+})
+.then(response => response.json())
+.then(data => {
+    console.log("Resposta do servidor:", data);
+})
+.catch(error => {
+    console.error("Erro ao criar conta:", error);
+});
 });
