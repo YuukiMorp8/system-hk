8822a51cf9b434002ce0bf90bc6b955fc04e7c53
Morfeus <kuthykymaell@gmail.com>
2026-04-17 21:29:56 -0300

    Update Script.js

--- Sistema/Script.js
+++ Sistema/Script.js
@@ -1,1 +1,11 @@
 
+let usuarioinput = document.getElementById("usuario");
+let senhainput = document.getElementById("senha");
+let loginbutton = document.getElementById("loginbutton");
+
+loginbutton.addEventListener("click", function() {
+    let usuario = usuarioinput.value;
+    let senha = senhainput.value;  
+    console.log("Usuário:", usuario);
+    console.log("Senha:", senha);
+});
