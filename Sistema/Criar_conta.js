770a3058b5dd567202e86634decf1a7dc37bc848
Morfeus <kuthykymaell@gmail.com>
2026-04-18 01:02:44 -0300

    Update Criar_conta.js

--- Sistema/Criar_conta.js
+++ Sistema/Criar_conta.js
@@ -1,1 +1,13 @@
+let nomeinput = document.getElementById("nome");
+let emailinput = document.getElementById("email");
+let senhainput = document.getElementById("senha");
+let criarconta = document.getElementById("btnCriarConta");
 
+criarconta.addEventListener("click", function() {
+    let nome = nomeinput.value;
+    let email = emailinput.value; 
+    let senha = senhainput.value; 
+    console.log("Nome:", nome);
+    console.log("Email:", email);
+    console.log("Senha:", senha);
+});
